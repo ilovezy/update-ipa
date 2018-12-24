463080f75f5c131e4dcf088ff9f7beec03517326
ilovezy <1518265554@qq.com>
2018-12-24 15:33:26 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <!-- <router-view/> -->
+    <div>
+      <a style="font-size: 30px;" href="itms-services://?action=download-manifest&url=https://read-public-test.oss-cn-hangzhou.aliyuncs.com/manifest.plist">ipa下载地址</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
